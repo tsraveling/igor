d83a627c607b7509e1e3dfcf58354afe1f53fb1b
Tim Raveling <tsraveling@proton.me>
2025-12-24 13:33:18 -0700

    feat: first code scaffolding for processor

--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"fmt"
+
 	tea "github.com/charmbracelet/bubbletea"
 )
 
 func main() {
 
 	var m tea.Model
-	m, _ = makeSomeModel()
+	m, _ = makeProcessModel()
 
 	p := tea.NewProgram(m)
 	p.Run()
+
+	fmt.Printf(igorLogo)
 }
---
+++ mainProcess.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	tea "github.com/charmbracelet/bubbletea"
+	// "github.com/charmbracelet/lipgloss"
+)
+
+type processModel struct {
+	someValue string
+}
+
+func makeProcessModel() (processModel, tea.Cmd) {
+	m := processModel{someValue: "example"}
+	return m, m.Init()
+}
+
+func (m processModel) Init() tea.Cmd {
+	return nil
+}
+
+func (m processModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	//	case tea.WindowSizeMsg:
+	//		m.list.SetWidth(msg.Width)
+
+	case tea.KeyMsg:
+		switch keypress := msg.String(); keypress {
+		case "esc", "ctrl+c":
+			return m, tea.Quit
+		}
+	}
+
+	// Text input gets the end of it
+	// var cmd tea.Cmd
+	// m.input, cmd = m.input.Update(msg)
+	// return m, cmd
+
+	return m, nil
+}
+
+func (m processModel) View() string {
+	return "hello, world"
+}
+
+// var (
+// 	titleStyle = lipgloss.NewStyle().
+// 			Bold(true).
+// 			Foreground(lipgloss.Color("205")).
+// 			MarginLeft(2)
+//
+// 	itemStyle = lipgloss.NewStyle().
+// 			PaddingLeft(4)
+// )

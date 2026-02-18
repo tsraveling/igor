fefc7526e37611692c1af7b8136d04dcae8ea352
Tim Raveling <tsraveling@proton.me>
2026-02-18 16:51:43 -0700

    fix: assembles env file as well.

--- writer.go
+++ writer.go
@@ -50,6 +50,32 @@ func writeResourcesCmd(work []workPiece) tea.Cmd {
 			writeSpriteFrames(charName, parentPath, packs)
 		}
 
+		// Generate reassembly .tscn for env folders
+		envPacks := map[string][]workPack{}
+		envSlices := map[string][]workSlice{}
+		for _, w := range work {
+			switch v := w.(type) {
+			case workPack:
+				if v.f.typ == FolderTypeEnv {
+					envPacks[v.f.path] = append(envPacks[v.f.path], v)
+				}
+			case workSlice:
+				if v.f.typ == FolderTypeEnv {
+					envSlices[v.f.path] = append(envSlices[v.f.path], v)
+				}
+			}
+		}
+		envFolders := map[string]bool{}
+		for p := range envPacks {
+			envFolders[p] = true
+		}
+		for p := range envSlices {
+			envFolders[p] = true
+		}
+		for folderPath := range envFolders {
+			writeEnvTscn(folderPath, envPacks[folderPath], envSlices[folderPath])
+		}
+
 		return writingCompleteMsg{}
 	}
 }

7a52053c9c52f27c7a575f2422154a641d3f7c0a
Tim Raveling <tsraveling@proton.me>
2026-02-21 12:34:58 -0700

    fix: adds arg support to main

--- main.go
+++ main.go
@@ -1,16 +1,63 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"math/rand"
+	"os"
+	"strings"
+
 	tea "github.com/charmbracelet/bubbletea"
 )
 
 var prg *tea.Program
 
+const defaultConfig = `destination: ./out
+source: ./in
+spritesheet-size: 4096
+slice-size: 1024
+res-prefix: out/
+rules:
+  "chars/**": { mode: character }
+  "env/**": { mode: env }
+`
+
+func initProject() {
+	if _, err := os.Stat("igor.yml"); err == nil {
+		fmt.Println("igor.yml already exists in this directory.")
+		return
+	}
+
+	fmt.Print("Create igor.yml in the current directory? [y/N] ")
+	reader := bufio.NewReader(os.Stdin)
+	input, _ := reader.ReadString('\n')
+	input = strings.TrimSpace(strings.ToLower(input))
+
+	if input != "y" && input != "yes" {
+		fmt.Println("Cancelled.")
+		return
+	}
+
+	if err := os.WriteFile("igor.yml", []byte(defaultConfig), 0644); err != nil {
+		fmt.Printf("Error creating igor.yml: %s\n", err.Error())
+		return
+	}
+
+	fmt.Println("Created igor.yml.")
+}
+
 func main() {
 
-	// TODO: Replace this with arg[0]
-	err := loadProject("./test")
+	if len(os.Args) > 1 && os.Args[1] == "init" {
+		initProject()
+		return
+	}
+
+	dir := "."
+	if len(os.Args) > 1 {
+		dir = os.Args[1]
+	}
+	err := loadProject(dir)
 	if err != nil {
 		fmt.Printf("%s", err.Error())
 		return
@@ -22,5 +69,7 @@ func main() {
 	prg = tea.NewProgram(m)
 	prg.Run()
 
-	fmt.Printf(igorLogo)
+	byes := []string{"Those who are about to die salute you.", "Happy hunting!", "Vaya con quesos.", "May the wind be always at your back.", "Later, alligator.", "Actually, Frankenstein was the doctor"}
+	pick := byes[rand.Intn(len(byes))]
+	fmt.Printf("%s\n", pick)
 }
